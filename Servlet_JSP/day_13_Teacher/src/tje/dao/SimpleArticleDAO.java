9b6d9d3e5d40180841f4d2d32a075403de4056ee
KimHeonSeung <47492073+KimHeonSeung@users.noreply.github.com>
2019-06-13 18:19:09 +0900

    Day_13_Teacher_2019.06.13.

---
+++ Servlet_JSP/day_13_Teacher/src/tje/dao/SimpleArticleDAO.java
@@ -0,0 +1,57 @@
+package tje.dao;
+
+import java.sql.*;
+import java.util.*;
+import java.util.Date;
+
+import tje.jdbc.util.*;
+import tje.model.*;
+
+public class SimpleArticleDAO {
+	private SimpleArticle getInstance(ResultSet rs) throws SQLException {		
+		SimpleArticle obj = new SimpleArticle(
+				rs.getInt("article_id"),
+				rs.getString("title"),
+				rs.getString("member_id"),
+				rs.getString("name"),
+				rs.getTimestamp("write_time"),
+				rs.getInt("read_count"));
+		return obj;
+	}
+	
+	public ArrayList<SimpleArticle> selectAll(Connection conn) {
+		ArrayList<SimpleArticle> result = new ArrayList<>();
+		String sql = "select * from simpleArticle";
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sql);						
+			rs = pstmt.executeQuery();
+			
+			while( rs.next() )
+				result.add(getInstance(rs));
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}
+		
+		Closer.close(rs);
+		Closer.close(pstmt);
+		
+		return result;
+	}	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
