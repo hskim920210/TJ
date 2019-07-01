45ce8314ac466d574bdb4d3bf760a54dcc084538
KimHeonSeung <47492073+KimHeonSeung@users.noreply.github.com>
2019-07-01 18:05:56 +0900

    Day_01_2019.07.01.

---
+++ Spring/first/src/test/java/com/tje/first/SampleTest.java
@@ -0,0 +1,13 @@
+package com.tje.first;
+
+import junit.framework.TestCase;
+
+public class SampleTest extends TestCase {
+
+	public void testReverse() {
+		String actual = Sample.reverse("hello");
+		String expected = "olleh";
+		assertEquals(expected, actual);
+	}
+
+}
